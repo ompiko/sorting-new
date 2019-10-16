c2e4857fa97d28689c0ed4e5979fe330bdaa1511
mochamadsyafiq_ <mochamadsyafiq02@gmail.com>
2019-10-16 12:30:09 +0700

    second commit

--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -1,4 +1,5 @@
 package sorting;
+
 /**
  *
  * @author ASUS
@@ -8,8 +9,36 @@ public class Sorting {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;//searching for lowest index  
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
     }
-    
+
+    static void display(int arr[]) {
+        System.out.println("After Selection Sort");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String a[]) {
+        int[] arr1 = {93, 69, 99, 46, 27, 21, 26};
+        System.out.println("Before Selection Sort");
+        for (int i : arr1) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        
+        selectionSort(arr1);//sorting array using selection sort  
+        display(arr1);
+    }
 }
